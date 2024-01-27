969b6d9ee165cf18d2756f01c9cd609680279f36
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-27 23:06:48 +0530

    Update Q3_Answer.sql

--- Week_1_Hw/Q3_Answer.sql
+++ Week_1_Hw/Q3_Answer.sql
@@ -5,3 +5,5 @@ FROM
 WHERE
   lpep_pickup_datetime BETWEEN '2019-09-18 00:00:00' AND '2019-09-18 23:59:59'
 AND lpep_dropoff_datetime BETWEEN '2019-09-18 00:00:00' AND '2019-09-18 23:59:59';
+
+-- Answer :- 15612
