1ae71b71d4551aa2c31a3bf9e0ee49155710740f
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-27 23:01:58 +0530

    Create Q4_Answer.sql

---
+++ Week_1_Hw/Q4_Answer.sql
@@ -0,0 +1,8 @@
+SELECT
+  lpep_pickup_datetime::date AS pickup_day,
+  MAX(trip_distance) AS max_trip_distance
+FROM
+  green_taxi_data_sept
+GROUP BY
+  pickup_day
+ORDER BY max_trip_distance DESC;
