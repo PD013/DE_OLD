6b0cac2b4575a42ab62593a3ab13dda71f142ba4
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-27 23:02:44 +0530

    Create Q5_Answer.sql

---
+++ Week_1_Hw/Q5_Answer.sql
@@ -0,0 +1,19 @@
+SELECT
+  T."Borough",
+  SUM(G.total_amount) AS total_amount_sum
+FROM
+  green_taxi_data_sept AS G
+INNER JOIN
+  taxi_zone_data AS T
+ON
+  G."PULocationID" = T."LocationID"
+WHERE
+  G.lpep_pickup_datetime::date = '2019-09-18'
+  AND T."Borough" != 'Unknown'
+GROUP BY
+  T."Borough"
+HAVING
+  SUM(G.total_amount) > 50000
+ORDER BY
+  total_amount_sum DESC
+LIMIT 3;
