6341440df1d1bf4a07b761116f61be9c324976fa
Preet Desai <114251906+PD013@users.noreply.github.com>
2024-01-27 23:01:35 +0530

    Create Q3_Answer.sql

---
+++ Week_1_Hw/Q3_Answer.sql
@@ -0,0 +1,7 @@
+SELECT
+  COUNT(*)
+FROM
+  green_taxi_data_sept
+WHERE
+  lpep_pickup_datetime BETWEEN '2019-09-18 00:00:00' AND '2019-09-18 23:59:59'
+AND lpep_dropoff_datetime BETWEEN '2019-09-18 00:00:00' AND '2019-09-18 23:59:59';
